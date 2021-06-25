a0c19728ea3a68becc72a4929031ad955805b532
Ashika <ashika@Venkateswars-MacBook-Pro.local>
2021-06-26 01:49:24 +0530

    dont use yet

---
+++ download_data.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+mkdir ./Data
+cd Data
+
+declare -a taxonomic_group=( "viral" "archaea" "bacteria" "fungi" "invertebrate/Caenorhabditis_elegans" "invertebrate/Drosophila_melanogaster/" "vertebrate_other/Xenopus_tropicalis/" "vertebrate_mammalian/Mus_musculus" "vertebrate_mammalian/Homo_sapiens" )
+
+for i in "${taxonomic_group[@]}"
+do
+
+curl 'ftp://ftp.ncbi.nlm.nih.gov/genomes/refseq/${taxonomic_group}/assembly_summary.txt' | \
+awk '{FS="\t"} !/^#/ {print $20} ' | \
+sed -r 's|(ftp://ftp.ncbi.nlm.nih.gov/genomes/all/.+/)(GCF_.+)|\1\2/\2_genomic.fna.gz|' > genomic_file
+
+mkdir ${taxonomic_group[@]}
+cd ${taxonomic_group[@]}
+wget --input genomic_file
+cd ..
+
+done
