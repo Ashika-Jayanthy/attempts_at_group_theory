76f035aaff74e3ea21deb184c2d18f481707cef2
Ashika-Jayanthy <jayanthyashika@gmail.com>
2021-07-03 02:36:00 +0530

    not right just an update

--- main_v2.py
+++ main_v2.py
@@ -1,6 +1,19 @@
 import numpy as np
 from scipy.linalg import expm
-import math
+
+def matrix_multiply(a,b):
+    n1,m1 = a.shape
+    n2,m2 = b.shape
+    ans = np.zeros((n1,m2),dtype="complex128")
+    for row in range(n1):
+        row_value = a[row]
+        for column in range(m2):
+            column_value = b.T[column]
+            ans[row,column] = np.vdot(row_value,column_value)
+    return ans
+
+def commutator(a,b):
+    return matrix_multiply(a,b)
 
 ####
 s = 4
@@ -42,11 +55,9 @@ def random_sequence_evolve(sequence,l_replacement=1):
     new_sequence = sequence[:replacement_index] + replacement + sequence[replacement_index + l_replacement:]
     return new_sequence
 
-def Y(t,y):
-    g1 = Sequence(sequence_array[int(t)]).run()
-    gg1 = expm(g1)
-    g_t = np.array([gg1[0,0], gg1[1,0]])
-    return g_t
+def Y(y,t):
+    y_t = matrix_multiply(y,Sequence(sequence_array[t]).run())
+    return y_t
 
 
 sequence_array = []
@@ -59,17 +70,25 @@ for t in range(T):
 
 
 y0 = Sequence(init_sequence).run()
-y = y0
+y_array = []
 
 for n in range(n_sequences):
-    I1, k1 = Y(y)
+    k = np.zeros(s)
+    I1, k[0] = Y(y,n)
 
     for i in range(2,s):
+        u = np.zeros(s-2,2,2)
+        u_tilda = np.zeros(s-2,2,2)
         u[i] = h * np.sum([A[i,j] * k[j] for j in range(i-1)])
         u_tilda[i] = u[i] + (((c[i] * h) / 6) * commutator(I1, u[i]))
-        k[i] = Y(y * expm(u_tilda[i]))
-        
-    I2 = ((m1 * (k2 - I1)) + (m2 * (k3 - I1)) + (m3 * (k4 - h))) / h
+        k[i-1] = Y(matrix_multiply(y, expm(u_tilda[i])), n)
+
+    v = np.zeros(s-2,2,2)
+    v_tilda = np.zeros(s-2,2,2)
+    I2 = ((m1 * (k[1] - I1)) + (m2 * (k[2] - I1)) + (m3 * (k[3] - h))) / h
     v = h * np.sum([b[j] * k[j] for j in range(s)])
     v_tilda = v + ((h / 4) * commutator(I1,v)) + ((h**2 / 24) * commutator(I2,v)
-    y = y * expm(u_tilda[i])
+    y = matrix_multiply(y, expm(v_tilda))
+    y_array.append(y)
+
+print(y_array)
