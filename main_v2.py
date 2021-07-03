17339b3b9b3ba66fc4eab76ecd7623b2fb39d310
Ashika-Jayanthy <jayanthyashika@gmail.com>
2021-07-03 18:37:10 +0530

    ---

--- main_v2.py
+++ main_v2.py
@@ -27,8 +27,8 @@ def condition_check(val, type="matrix"):
 
 ####
 s = 4
-h = .000001
-n_sequences = 10
+h = .0000001
+n_sequences = 60
 
 
 A = np.array(
@@ -66,10 +66,14 @@ def random_sequence_evolve(sequence,l_replacement=1):
     new_sequence = sequence[:replacement_index] + replacement + sequence[replacement_index + l_replacement:]
     return new_sequence
 
+"""
 def Y(y,t):
     y_t = Sequence(sequence_array[t]).run()
     return expm(y_t)
+"""
 
+def Y(y,t):
+    return 2*y*t
 
 sequence_array = []
 init_sequence = ''.join(random.choice('CGTA') for _ in range(7))
@@ -80,9 +84,14 @@ for t in range(n_sequences):
     sequence_array.append(ss)
 
 
-y = expm(Sequence(init_sequence).run())
+#y = expm(Sequence(init_sequence).run())
+
+y = expm(np.array([[0,complex(0,1)],[complex(0,1),0]]))
 y_array = []
 y_array.append(y)
+
+
+
 
 for n in range(n_sequences):
     k = np.zeros((s,2,2), dtype="complex128")
@@ -92,14 +101,15 @@ for n in range(n_sequences):
     for i in range(0,2):
         u = np.zeros((s-2,2,2), dtype="complex128")
         u_tilda = np.zeros((s-2,2,2), dtype="complex128")
+
         u[i] = h * np.sum([A[i+2,j] * k[j] for j in range(i)], axis=0)
-        u_tilda[i] = u[i] + (((c[i+2] * h) / 6) * commutator(I1, u[i]))
+        u_tilda[i] = u[i] + (((c[i+2] * h) / 6.) * commutator(I1, u[i]))
         k[i+1] = Y(matrix_multiply(y, expm(u_tilda[i])), n)
 
 
     I2 = ((m1 * (k[1] - I1)) + (m2 * (k[2] - I1)) + (m3 * (k[3] - h))) / h
     v = h * np.sum([b[j] * k[j] for j in range(s)], axis=0)
-    v_tilda = v + ((h / 4) * commutator(I1,v)) + ((h**2 / 24) * commutator(I2,v))
+    v_tilda = v + ((h / 4.) * commutator(I1,v)) + ((h**2 / 24.) * commutator(I2,v))
     y = matrix_multiply(y, expm(v_tilda))
     y_array.append(y)
 
