1fb34727cd0330879b93f308d48a6d96def10e4e
Ashika-Jayanthy <jayanthyashika@gmail.com>
2021-07-06 13:50:09 +0530

    .

--- human_viruses.py
+++ human_viruses.py
@@ -1,8 +1,22 @@
 from Bio import pairwise2
-import pyhmmer
 from Bio import SeqIO
 from core import *
+import glob
+import sys
+
+indir = "./Data/hmm_sequences/"
 
 def pairwise_alignment(s1,s2):
-    score = pairwise2.align.globalxx("ACCGT", "ACG", score_only=True, one_alignment_only=True)
+    score = pairwise2.align.globalxx(s1, s2, score_only=True, one_alignment_only=True)
     return score
+
+files = glob.glob(f"{indir}/*")
+
+
+for file in files:
+    sequences = []
+    for record in SeqIO.parse(file,"fasta"):
+        sequences.append(record.seq)
+    distances
+    for i in range(len(sequences)):
+        for j in range(i+1,len(sequences)):
