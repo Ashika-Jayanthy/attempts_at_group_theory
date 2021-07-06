ce9fbec6a3f48c5b13ac56364bf08ebdff279c4e
Ashika-Jayanthy <jayanthyashika@gmail.com>
2021-07-07 03:18:40 +0530

    .

--- human_viruses.py
+++ human_viruses.py
@@ -5,10 +5,11 @@ import glob
 import sys
 import numpy as np
 import random
+import threading
 
 indir = "./Data/hmm_sequences/"
 outdir = "./Data/yarrays"
-
+files = glob.glob(f"{indir}/*")
 
 def matrix_to_c2(m):
     return np.array([m[0,0],m[1,0]])
@@ -17,48 +18,43 @@ def pairwise_alignment(s1,s2):
     score = pairwise2.align.globalxx(s1, s2, score_only=True, one_alignment_only=True)
     return score
 
-files = glob.glob(f"{indir}/*")
-
-
-
-for ff,file in enumerate(files):
-
-    print(f"{ff} of {len(files)}")
-    sequences = []
-    for record in SeqIO.parse(file,"fasta"):
-        sequences.append(record.seq.upper())
-
-    n = len(sequences)
-    distances = np.zeros((n,n))
-    for i in range(n):
-        for j in range(i+1,n):
-            align_score = pairwise_alignment(sequences[i],sequences[j])
-            distances[i,j] = align_score
-            distances[j,i] = align_score
-
-    ordered_sequences = []
-    idx = random.choice(np.arange(n))
-    distances[:,idx] = 0
-    ordered_sequences.append(sequences[idx])
-    while len(ordered_sequences)<len(sequences):
-        idx = np.argmax(distances[idx])
+def per_thread(start,stop):
+    ff = start
+    for file in files[start:stop]:
+        print(f"{ff} of {start}:{stop}")
+        sequences = []
+        for record in SeqIO.parse(file,"fasta"):
+            sequences.append(record.seq.upper())
+        n = len(sequences)
+        distances = np.zeros((n,n))
+        for i in range(n):
+            for j in range(i+1,n):
+                align_score = pairwise_alignment(sequences[i],sequences[j])
+                distances[i,j] = align_score
+                distances[j,i] = align_score
+        ordered_sequences = []
+        idx = random.choice(np.arange(n))
         distances[:,idx] = 0
         ordered_sequences.append(sequences[idx])
+        while len(ordered_sequences)<len(sequences):
+            idx = np.argmax(distances[idx])
+            distances[:,idx] = 0
+            ordered_sequences.append(sequences[idx])
+        def Y(t):
+            seq = ordered_sequences[t]
+            y_t = expm(Sequence(seq).run())
+            return y_t
+        n_sequences = len(ordered_sequences)
+        y_array = []
+        for n in range(n_sequences):
+            y = Y(n)
+            next_y = rkmk_step(Y,y,n)
+            y_array.append(matrix_to_c2(next_y))
+        np.savetxt(f"{outdir}/f{ff}_yarray.txt", y_array, fmt='%.18e', delimiter=' ', newline='\n')
+        ff+=1
+        return
 
 
-    def Y(t):
-        seq = ordered_sequences[t]
-        y_t = expm(Sequence(seq).run())
-        return y_t
-
-
-    n_sequences = len(ordered_sequences)
-    y_array = []
-
-    for n in range(n_sequences):
-        y = Y(n)
-        next_y = rkmk_step(Y,y,n)
-        y_array.append(matrix_to_c2(next_y))
-
-
-    np.savetxt(f"{outdir}/f{ff}_yarray.txt", y_array, fmt='%.18e', delimiter=' ', newline='\n')
+for start_file_num in range(0,1000,50):
+    thread = threading.Thread(target=per_thread, name = str(start_file_num), args=(start_file_num,start_file_num+50))
+    thread.start()
