0e5135e04aba2aac1b9805d3ce5b7333117f25f5
Ashika-Jayanthy <jayanthyashika@gmail.com>
2021-07-08 01:24:54 +0530

    .

--- background_random.py
+++ background_random.py
@@ -7,7 +7,7 @@ import random
 indir = "./Data/hmm_sequences/"
 outdir = "./Data/background_yarrays"
 
-
+y0 = np.array([[complex(0,1),complex(0,0)],[complex(0,0),complex(0,0)]])
 
 def DNA(l):
     return ''.join([random.choice('CGTA') for i in range(l)])
@@ -22,18 +22,17 @@ n_sequences = len(lengths)
 for ff in range(1000):
     print(f"{ff} of 1000")
 
-    def Y(t):
+    def Y(y,t):
         seq = DNA(lengths[t])
-        y_t = expm(Sequence(seq).run())
+        y_t = matrix_multiply(y,expm(Sequence(seq).run()))
         return y_t
 
     y_array = []
-
+    y = Y(y0,0)
     for n in range(n_sequences):
-        y = Y(n)
-        next_y = rkmk_step(Y,y,n)
-        y_array.append(next_y)
+        y = rkmk_step(Y,y,n)
+        y_array.append(y)
 
-
-    for i in range(len(y_array)):
-        np.savetxt(f"{outdir}/f{ff}_{i}_yarray.txt", y_array[i], fmt='%.18e', delimiter=' ', newline='\n')
+    print(f"{file_num} Writing output..")
+    with open(f"{outdir}/f{file_num}_backgroundyarray.pkl",'wb') as outfile:
+        pkl.dump(y_array,outfile)
