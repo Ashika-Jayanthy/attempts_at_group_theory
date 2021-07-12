a574ef189f5518e24058d411c53ff35ab59bf99f
Ashika-Jayanthy <jayanthyashika@gmail.com>
2021-07-12 13:37:54 +0530

    .

--- background_random.py
+++ background_random.py
@@ -1,21 +1,20 @@
 from Bio import SeqIO
 from core import *
-import glob
 import numpy as np
 import random
+import pickle as pkl
 
 indir = "./Data/hmm_sequences/"
 outdir = "./Data/background_yarrays"
 
-y0 = np.array([[complex(0,1),complex(0,0)],[complex(0,0),complex(0,0)]])
+y0 = np.array([[complex(1,0),complex(0,0)],[complex(0,0),complex(0,0)]])
 
 def DNA(l):
     return ''.join([random.choice('CGTA') for i in range(l)])
 
-files = glob.glob(f"{indir}/*")
-lengths = []
 
-for record in SeqIO.parse(files[0],"fasta"):
+lengths = []
+for record in SeqIO.parse(f"./Data/hmm_sequences/1.fasta","fasta"):
     lengths.append(len(record.seq))
 n_sequences = len(lengths)
 
@@ -24,15 +23,16 @@ for ff in range(1000):
 
     def Y(y,t):
         seq = DNA(lengths[t])
-        y_t = matrix_multiply(y,expm(Sequence(seq).run()))
+        alg = Sequence(seq).run()
+        y_t = matrix_multiply(y,expm(alg))
         return y_t
 
     y_array = []
-    y = Y(y0,0)
+    y = y0
     for n in range(n_sequences):
         y = rkmk_step(Y,y,n)
         y_array.append(y)
 
-    print(f"{file_num} Writing output..")
-    with open(f"{outdir}/f{file_num}_backgroundyarray.pkl",'wb') as outfile:
+    print(f"{ff} Writing output..")
+    with open(f"{outdir}/f{ff}_backgroundyarray.pkl",'wb') as outfile:
         pkl.dump(y_array,outfile)
