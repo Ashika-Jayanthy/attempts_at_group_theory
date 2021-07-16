b30ac39e05f8509d3e1aab04699eefdc8dd729f8
Ashika-Jayanthy <jayanthyashika@gmail.com>
2021-07-16 13:31:32 +0530

    .

--- run_rkmk.py
+++ run_rkmk.py
@@ -7,8 +7,11 @@ indir = "./Data/ordered_sequences"
 outdir = "./Data/yarrays"
 y0 = np.array([[complex(0.5,0.5),complex(1,1)],[complex(0.5,0.5),complex(1,1)]])
 
+all_y = np.zeros((1000,20,2,2),dtype="complex128")
 
 for i in range(1,1001):
+    print(f"{i} of 1000")
+    
     with open(f"{indir}/f{i}_ordered_sequence.pkl",'rb') as michaelscott:
         ordered_sequences = pkl.load(michaelscott)
 
@@ -20,13 +23,14 @@ for i in range(1,1001):
         y_t = matrix_multiply(y,expm(alg))
         return y_t
 
-    y_array = []
+    y_array = np.zeros((20,2,2),dtype="complex128")
     y = y0
     for n in range(n_sequences):
         y = rkmk_step(Y,y,n)
-        y_array.append(y)
+        y_array[n] = y
 
-    print(y_array)
-    #print(f"{i} Writing output..")
-    #with open(f"{outdir}/f{i}_yarray.pkl",'wb') as outfile:
-        #pkl.dump(y_array,outfile)
+    all_y[i] = y_array
+
+print(f"{i} Writing output..")
+with open(f"{outdir}/virus_all_y.pkl",'wb') as outfile:
+    pkl.dump(all_y,outfile)
